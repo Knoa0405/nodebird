c4c324072a47d5dd1490da7fd117221483cef378
Noah <mike1124@naver.com>
2021-02-09 21:25:45 +0900

    ⚡️ Connect devTools and configureStore

--- store/configureStore.js
+++ store/configureStore.js
@@ -1,15 +1,18 @@
 import { createWrapper } from 'next-redux-wrapper';
-import { createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducer from '../reducers';
 
 const configureStore = () => {
-  const store = createStore(reducer);
-  store.dispatch({
-    type : 'CHANGE_NICKNAME',
-    data : 'boogi'
-  });
+  const middlewares = [];
 
+  const enhancer = process.env.NODE_ENV === 'production'
+    ? compose(applyMiddleware(...middlewares))
+    : composeWithDevTools(applyMiddleware(...middlewares));
+
+  const store = createStore(reducer,enhancer);
+  
   return store;
 };
 
