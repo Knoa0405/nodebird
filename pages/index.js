048170fbbf1768a85d846db74dc7cba77e63e5f7
Noah <mike1124@naver.com>
2021-02-10 20:40:46 +0900

    🎨 Implements post Form

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import AppLayout from '../components/Applayout';
 
 const Home = () => {
+  const { isLoggedIn } = useSelector((state) => state.user);
+  const { mainPosts } = useSelector((state) => state.post);
   return (
     <AppLayout>
-      <div>Home</div>
+      {isLoggedIn && <PostForm/>}
+      {mainPosts.map((post) => <PostCard key={post.id} post={post}/>)}
     </AppLayout>
   );
 };
