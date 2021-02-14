2a5f402644457643e86c97d8412457021ee4c78c
Noah <mike1124@naver.com>
2021-02-14 20:11:06 +0900

    🎨 Add Comment about redux saga

--- sagas/index.js
+++ sagas/index.js
@@ -7,8 +7,12 @@ function logInAPI () {
 
 function* logIn() {
   try {
+    // call 동기 blocking await 와 같은 느낌?
+    // axios.post().then(~) 느낌
+    // fork 비동기 non-blocking 함수 호출 실행 후 그다음 줄로 넘어가 실행됨.
+    // axios.post(~) 느낌
     const result = yield call(logInAPI);
-
+    // put 은 dispatch 라 생각
     yield put({
       type : 'LOG_IN_SUCCESS',
       data : result.data
@@ -20,7 +24,7 @@ function* logIn() {
     });
   }
 }
-
+// 이벤트 리스너 같은 느낌?
 function* watchLogin() {
   yield take('LOG_IN_REQUEST' ,logIn);
 }
